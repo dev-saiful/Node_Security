ac7e40c73100c362aca59bbbaad9e849add21ebb
dev-saiful <saifulcseian@gmail.com>
2023-12-19 20:11:40 +0600

    helmet install and https added

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
+const fs = require("fs");
+const https = require("https");
 const path = require("path")
 const express = require("express");
-const app = express();
+const helmet = require("helmet");
 
+const app = express();
 const PORT = 3000;
+
+app.use(helmet());
+
+app.get("/secret",(req,res)=>{
+    res.send("This is secret");
+})
 
 app.get("/",(req,res)=>{
     res.sendFile(path.join(__dirname,"public","index.html"));
 })
 
-app.listen(PORT,()=>{
+https.createServer({
+    key:fs.readFileSync("key.pem"),
+    cert:fs.readFileSync("cert.pem"),
+},app).listen(PORT,()=>{
     console.log("Server running on PORT:3000");
 })
