653d764beb8ba176849ca3d29cf55d0a518210f3
dev-saiful <saifulcseian@gmail.com>
2023-12-21 20:32:52 +0600

    auth endpoint added

--- server.js
+++ server.js
@@ -9,17 +9,42 @@ const PORT = 3000;
 
 app.use(helmet());
 
-app.get("/secret",(req,res)=>{
+function checkLoggedIn(req,res,next)
+{
+    const isLoggedIn = true;
+    if(!isLoggedIn)
+    {
+        return res.status(401).json({
+            error:"Must be Log in",
+        });
+    }
+    next();
+}
+
+app.get("/secret",checkLoggedIn,(req,res)=>{
     res.send("This is secret");
-})
+});
+
+app.get("/auth/google",(req,res)=>{
+
+});
+
+app.get("/auth/google/callback",(req,res)=>{
+
+});
+
+app.get("/auth/logout",(req,res)=>{
+
+});
+
 
 app.get("/",(req,res)=>{
     res.sendFile(path.join(__dirname,"public","index.html"));
-})
+});
 
 https.createServer({
     key:fs.readFileSync("key.pem"),
     cert:fs.readFileSync("cert.pem"),
 },app).listen(PORT,()=>{
     console.log("Server running on PORT:3000");
-})
+});
